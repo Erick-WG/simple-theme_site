1d0702b6edc890352704dd197344abc39eb71a94
Erick Wainaina <116720470+Erick-WG@users.noreply.github.com>
2024-04-30 20:18:19 +0300

    initial commits
    added the site files containing the script file (controls the toggle, for changing the content on the site [the button text] ), the style sheet (contains the styles for the mini app), the markup (body skeleton containing all the child nodes being modified and altered.

---
+++ theme-site/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const themeButton = document.querySelector('.theme-btn')
+
+
+themeButton.addEventListener('click', ()=>{
+    // the className has to be updated with every function call ('clicks'), hence should be stored within the eventListener.
+    const className = document.body.className
+
+    document.body.classList.toggle('light-theme')
+    document.body.classList.toggle('dark-theme')
+
+
+    // condition to control the button text based on the theme.
+    className == 'light-theme' ? themeButton.textContent = 'Dark' : themeButton.textContent = 'Light'
+
+    console.log('Current class name: '+ className)
+
+});
+
+
+
+
+
+
